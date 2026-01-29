181706387122bcc09c398cf8cf2828eec1c66d14
Harold Saenz <joshu4nick19@gmail.com>
2026-01-28 22:04:59 -0500

    comments on myAccount.js for testing environment (local or server)

--- myAccount.js
+++ myAccount.js
@@ -2,9 +2,20 @@ const current_name = sessionStorage.getItem("name");
 const userID = sessionStorage.getItem("userID");
 
 // Debugging to check if info is being pulled
+
 //console.log("Debug - Name in storage:", current_name);
 //console.log("Debug - ID in storage:", userID);
 
+// **************************NOTE***********************************************
+// REMOVE the comments on USER CHECK for the server version to work
+
+// IF working on LOCAL keep USER CHECK as comments since you wont be able to 
+// access account.html without verifying user
+
+
+
+// USER CHECK
+/*
 if(!userID){
     window.location.href = "index.html";
 
@@ -13,3 +24,4 @@ else{
     document.getElementById("accountName").innerHTML = current_name || "User"
 }
 
+*/
